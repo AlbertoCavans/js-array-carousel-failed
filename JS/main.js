1302c9ef9ff65f348d6cf2038e5c4af8716394fa
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-19 12:52:08 +0100

    Feat JS - slides progress

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,26 @@
 /* LINKS TO HTML */
-const carouselBox = document.getElementsByClassName("carousel-box");
+const carouselBox = document.getElementById("carousel-box");
 
 /* ARRAY */
 const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 
+/*  SLIDES */
 let slidesHTMLimg = "";
 for (let i = 0; i < slides.length; i++) {
   const imgSlide = slides[i];
 
   let classActive;
-
   if (i == 0) {
     classActive = "active";
   } else {
     classActive = "";
   }
-  slidesHTMLimg += `<img src="./img/${imgSlide}" alt="slide${i}" class="slide ${classActive}" />`;
+
+  slidesHTMLimg = `<img src="./img/${imgSlide}" alt="slide${i}" class="slide ${classActive}"/>`;
 }
 
 carouselBox.innerHTML = slidesHTMLimg;
+
+/* BUTTON UP */
+
+/* BUTTON DOWN */
