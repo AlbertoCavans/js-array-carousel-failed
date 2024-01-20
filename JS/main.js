15748a81255072f71760b77a5c06226f5500a1d5
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-20 20:19:01 +0100

    Feat JS - Buttons

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,22 @@
 const carouselBox = document.getElementById("carousel-box");
 const arrowUp = document.getElementById("arrowUp");
 const arrowDown = document.getElementById("arrowDown");
+const imgBox = document.getElementById("img-box");
+let visualizedImg = 0;
 
 /* ARRAY */
 const slides = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 
 /*  SLIDES */
-let slidesHTMLimg = "";
+
+const visualizedImgUrl = slides[visualizedImg];
+const imgHtml = `<img src="./img/${visualizedImgUrl}" alt="slide${
+  visualizedImg + 1
+}" class="slide"/>`;
+
+carouselBox.innerHTML = imgHtml;
+
+let sildesHtml = "";
 for (let i = 0; i < slides.length; i++) {
   const imgSlide = slides[i];
 
@@ -18,13 +28,51 @@ for (let i = 0; i < slides.length; i++) {
     classActive = "";
   }
 
-  slidesHTMLimg = `<img src="./img/${imgSlide}" alt="slide${i}" class="slide ${classActive}"/>`;
+  slidesHTML = `<div class="d-flex align-items-center ${classActive}" id="img-box"><img src="./img/${imgSlide}" class="slide"/></div>`;
 }
 
-carouselBox.innerHTML = slidesHTMLimg;
+imgBox.innerHTML = slidesHTML;
+
+/* BUTTON DOWN */
+
+arrowDown.addEventListener("click", function () {
+  const allImgs = document.querySelector(img);
+  const prevImg = carouselBox[visualizedImg];
+  prevImg.classList.remove("active");
+
+  visualizedImg++;
+
+  const nextImg = allImgs[classActive];
+  nextImg.classList.add("active");
+
+  const visualizedImgUrl = slides[visualizedImg];
+  const imgHtml = `<img src="./img/${visualizedImgUrl}" alt="slide${
+    visualizedImg + 1
+  }" class="slide"/>`;
+
+  carouselBox.innerHTML = imgHtml;
+
+  if (classActive == slides.length - 1) this.classList.add("d-none");
+});
 
 /* BUTTON UP */
 
-arrowUp.addEventListener("click", function () {});
+arrowUp.addEventListener("click", function () {
+  const allImgs = document.querySelector(img);
+  const prevImg = carouselBox[visualizedImg];
+  prevImg.classList.remove("active");
 
-/* BUTTON DOWN */
+  visualizedImg--;
+
+  const nextImg = allImgs[classActive];
+  nextImg.classList.add("active");
+
+  const visualizedImgUrl = slides[visualizedImg];
+  const imgHtml = `<img src="./img/${visualizedImgUrl}" alt="slide${
+    visualizedImg + 1
+  }" class="slide"/>`;
+
+  carouselBox.innerHTML = imgHtml;
+
+  if (classActive === 0) this.classList.add("d-none");
+});
